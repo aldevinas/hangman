088f50e1c4ce42ed0165549186c25cba8424d0b9
unknown <aldis.box@gmail.com>
2021-03-12 15:52:25 +0200

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const hiddenWords = ["father", "snow", "chips", "data", "car", "bicycle",
+  "sun", "letter", "keyboard", "dog", "bird", "home", "book", "fireworks",
+  "smartphone", "restaurant"];
+
+let word = hiddenWords[Math.floor(Math.random() * hiddenWords.length)];
